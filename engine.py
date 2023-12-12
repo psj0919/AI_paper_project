6b13e4d824f5c4d22aaa4f187a82175e349cf31a
psj0919 <110454977+psj0919@users.noreply.github.com>
2023-12-12 15:44:22 +0900

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,8 +1,11 @@
 from model import SentimentLSTM, TransformerModel, Seq2SeqTransformer
+from transformers  import BertTokenizer, BertForSequenceClassification
 import re
 import torch
 import torch.nn as nn
+from torch.nn.functional import softmax
 import tqdm
+from PyKomoran import *
 from nltk.tokenize import sent_tokenize, word_tokenize
 import spacy
 
@@ -17,13 +20,11 @@ class Trainer():
         self.train_loader = self.get_dataloader()
         self.loss = self.setup_loss()
 
-
     def setup_optimizer(self):
         if self.cfg['option']['optim'] == 'Adam':
             optimizer = torch.optim.Adam(self.model.parameters())
 
         return optimizer
-
 
     def get_dataloader(self):
         from dataset import paperdata
@@ -37,6 +38,8 @@ class Trainer():
             model = TransformerModel(self.cfg['dataset']['max_size'], embedding_dim=256, hidden_dim=512, nhead=4, num_encoder_layers=2, num_classes=1,dropout=0.5)
         elif self.cfg['model']['name'] == 'seq2seq':
             model = Seq2SeqTransformer(self.cfg['dataset']['max_size'], embedding_dim= 256, hidden_dim = 512, nhead = 4, num_layers = 2)
+        elif self.cfg['model']['name'] == 'bert':
+            model = BertForSequenceClassification.from_pretrained("bert-base-uncased")
         return model
 
     def setup_loss(self):
@@ -47,27 +50,43 @@ class Trainer():
 
 
     def map_tokens_to_indices(self, x):
-        x = re.sub(r"[^a-zA-Z0-9가-힣\s]", "", x)
-        x = x.lower()
         tokens = word_tokenize(x)
         vocab = {word: idx + 1 for idx, word in enumerate(set(tokens))}
         indexed_tokens = [vocab[token] for token in tokens]
 
-        return torch.tensor(indexed_tokens).unsqueeze(0)
+        return torch.tensor(indexed_tokens).unsqueeze(0), vocab
+
+    def summarize_text(self, text):
+        tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+        max_length = 512
+        inputs = tokenizer(text, return_tensors="pt", max_length=max_length, truncation=True, padding=True)
+        with torch.no_grad():
+            logits = model(**inputs).logits
+        probs = torch.softmax(logits, dim=-1)
+        summary_prob = probs[:, 0].item()
+        predict_class = torch.argmax(logits, dim=1).item()
+        summary_text = tokenizer.decode(inputs["input_ids"][0])
+
+        return summarize_text
+
 
     def training(self):
         model = self.model
         optimizer = self.optimizer
         train_loader = self.train_loader
-
+        komoran = Komoran("EXP")
 
         for i in range(self.cfg['solver']['epoch']):
             model.train()
             loss_sum = 0.0
             for i, data in enumerate(train_loader):
-                input = self.map_tokens_to_indices(data['orginal_text'])
-                target = self.map_tokens_to_indices(data['summary_text'])
+                input = komoran.get_plain_text(data['orginal_text'])
+                target = komoran.get_plain_text(data['summary_text'])
+                input, input_vocab = self.map_tokens_to_indices(input)
+                target, target_vocab = self.map_tokens_to_indices(data['summary_text'])
                 pred = model(input, target)
+                probe = softmax(pred, dim = -1)
+                max_prob_index = torch.argmax(probe, dim=-1)
+                decode_word = input[max_prob_index.item()]
                 
-
-                print(pred)
+        
